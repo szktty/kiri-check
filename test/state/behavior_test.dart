ae41b8366550cc13a209068f93f97b1a96f90e8d
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2024-05-15 16:41:50 +0900

    ボロネーゼ

--- test/state/behavior_test.dart
+++ test/state/behavior_test.dart
@@ -6,6 +6,34 @@ import 'package:kiri_check/src/state/state.dart';
 import 'package:kiri_check/src/top.dart';
 import 'package:test/test.dart';
 
+final class DependencyTestState extends State {}
+
+final class DependencyTest extends Behavior<State> {
+  @override
+  State createState() => DependencyTestState();
+
+  @override
+  List<Command<State>> generateCommands(State s) {
+    final a = Action<State>('a', (_) {});
+    final b = Action<State>('b', (_) {}, dependencies: [a]);
+    final c = Action<State>('c', (_) {}, dependencies: [b]);
+    return [a, b, c];
+  }
+}
+
+final class UnknownDependencyTest extends Behavior<State> {
+  @override
+  State createState() => DependencyTestState();
+
+  @override
+  List<Command<State>> generateCommands(State s) {
+    final a = Action<State>('a', (_) {});
+    final b = Action<State>('b', (_) {}, dependencies: [a]);
+    final c = Action<State>('c', (_) {}, dependencies: [b]);
+    return [b, c];
+  }
+}
+
 final class CircularDependencyTestState extends State {}
 
 final class CircularDependencyTest extends Behavior<State> {
@@ -14,15 +42,8 @@ final class CircularDependencyTest extends Behavior<State> {
 
   @override
   List<Command<State>> generateCommands(State s) {
-    final a = Action<State>(
-      'a',
-      (_) {},
-    );
-    final b = Action<State>(
-      'b',
-      (_) {},
-      dependencies: [a],
-    );
+    final a = Action<State>('a', (_) {});
+    final b = Action<State>('b', (_) {}, dependencies: [a]);
     a.addDependency(b);
     return [a, b];
   }
@@ -31,13 +52,25 @@ final class CircularDependencyTest extends Behavior<State> {
 void main() {
   KiriCheck.verbosity = Verbosity.verbose;
 
-  property('circular dependency', () {
-    forAllStates(
-      CircularDependencyTest(),
-      (_) {},
-      onCheck: (f) {
-        expect(f, throwsA(isA<CircularDependencyException>()));
-      },
-    );
+  group('dependency', () {
+    property('unknown dependency', () {
+      forAllStates(
+        UnknownDependencyTest(),
+        (_) {},
+        onCheck: (f) {
+          expect(f, throwsA(isA<CommandDependencyException>()));
+        },
+      );
+    });
+
+    property('detect circular dependency', () {
+      forAllStates(
+        CircularDependencyTest(),
+        (_) {},
+        onCheck: (f) {
+          expect(f, throwsA(isA<CommandDependencyException>()));
+        },
+      );
+    });
   });
 }
