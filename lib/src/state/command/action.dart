36e4a5e368e1113eeef755abf7ceaeba568f31b8
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2024-05-20 16:37:31 +0900

    魯肉飯

--- lib/src/state/command/action.dart
+++ lib/src/state/command/action.dart
@@ -1,29 +1,32 @@
 import 'package:kiri_check/kiri_check.dart';
 import 'package:kiri_check/src/arbitrary.dart';
+import 'package:kiri_check/src/random.dart';
 import 'package:kiri_check/src/state/command.dart';
 import 'package:kiri_check/src/state/state.dart';
 
 final class Action<T extends State, U> extends Command<T> {
   Action(
     super.description,
-    this.arbitrary,
-    this.action, {
+    Arbitrary<U> arbitrary,
+    void Function(T, U) action, {
     super.dependencies,
     super.canExecute,
     super.precondition,
     super.postcondition,
     super.nextState,
-  });
+  }) {
+    _arbitrary = arbitrary;
+    _action = action;
+  }
 
-  // TODO: private
-  final Arbitrary<U> arbitrary;
-  final void Function(T, U) action;
+  late final Arbitrary<U> _arbitrary;
+  late final void Function(T, U) _action;
 
   @override
   void execute(T state) {
-    final base = arbitrary as ArbitraryBase<U>;
-    final value = base.generate(state.random);
-    action(state, value);
+    final base = _arbitrary as ArbitraryBase<U>;
+    final value = base.generate(state.random as RandomContext);
+    _action(state, value);
   }
 }
 
@@ -54,5 +57,163 @@ final class Action2<T extends State, E1, E2> extends Action<T, (E1, E2)> {
           description,
           combine2(arbitrary1, arbitrary2, (a, b) => (a, b)),
           (s, args) => action(s, args.$1, args.$2),
+        );
+}
+
+final class Action3<T extends State, E1, E2, E3>
+    extends Action<T, (E1, E2, E3)> {
+  Action3(
+    String description,
+    Arbitrary<E1> arbitrary1,
+    Arbitrary<E2> arbitrary2,
+    Arbitrary<E3> arbitrary3,
+    void Function(T, E1, E2, E3) action, {
+    super.dependencies,
+    super.canExecute,
+    super.precondition,
+    super.postcondition,
+    super.nextState,
+  }) : super(
+          description,
+          combine3(arbitrary1, arbitrary2, arbitrary3, (a, b, c) => (a, b, c)),
+          (s, args) => action(s, args.$1, args.$2, args.$3),
+        );
+}
+
+final class Action4<T extends State, E1, E2, E3, E4>
+    extends Action<T, (E1, E2, E3, E4)> {
+  Action4(
+    String description,
+    Arbitrary<E1> arbitrary1,
+    Arbitrary<E2> arbitrary2,
+    Arbitrary<E3> arbitrary3,
+    Arbitrary<E4> arbitrary4,
+    void Function(T, E1, E2, E3, E4) action, {
+    super.dependencies,
+    super.canExecute,
+    super.precondition,
+    super.postcondition,
+    super.nextState,
+  }) : super(
+          description,
+          combine4(arbitrary1, arbitrary2, arbitrary3, arbitrary4,
+              (a, b, c, d) => (a, b, c, d)),
+          (s, args) => action(s, args.$1, args.$2, args.$3, args.$4),
+        );
+}
+
+final class Action5<T extends State, E1, E2, E3, E4, E5>
+    extends Action<T, (E1, E2, E3, E4, E5)> {
+  Action5(
+    String description,
+    Arbitrary<E1> arbitrary1,
+    Arbitrary<E2> arbitrary2,
+    Arbitrary<E3> arbitrary3,
+    Arbitrary<E4> arbitrary4,
+    Arbitrary<E5> arbitrary5,
+    void Function(T, E1, E2, E3, E4, E5) action, {
+    super.dependencies,
+    super.canExecute,
+    super.precondition,
+    super.postcondition,
+    super.nextState,
+  }) : super(
+          description,
+          combine5(arbitrary1, arbitrary2, arbitrary3, arbitrary4, arbitrary5,
+              (a, b, c, d, e) => (a, b, c, d, e)),
+          (s, args) => action(s, args.$1, args.$2, args.$3, args.$4, args.$5),
+        );
+}
+
+final class Action6<T extends State, E1, E2, E3, E4, E5, E6>
+    extends Action<T, (E1, E2, E3, E4, E5, E6)> {
+  Action6(
+    String description,
+    Arbitrary<E1> arbitrary1,
+    Arbitrary<E2> arbitrary2,
+    Arbitrary<E3> arbitrary3,
+    Arbitrary<E4> arbitrary4,
+    Arbitrary<E5> arbitrary5,
+    Arbitrary<E6> arbitrary6,
+    void Function(T, E1, E2, E3, E4, E5, E6) action, {
+    super.dependencies,
+    super.canExecute,
+    super.precondition,
+    super.postcondition,
+    super.nextState,
+  }) : super(
+          description,
+          combine6(arbitrary1, arbitrary2, arbitrary3, arbitrary4, arbitrary5,
+              arbitrary6, (a, b, c, d, e, f) => (a, b, c, d, e, f)),
+          (s, args) =>
+              action(s, args.$1, args.$2, args.$3, args.$4, args.$5, args.$6),
+        );
+}
+
+final class Action7<T extends State, E1, E2, E3, E4, E5, E6, E7>
+    extends Action<T, (E1, E2, E3, E4, E5, E6, E7)> {
+  Action7(
+    String description,
+    Arbitrary<E1> arbitrary1,
+    Arbitrary<E2> arbitrary2,
+    Arbitrary<E3> arbitrary3,
+    Arbitrary<E4> arbitrary4,
+    Arbitrary<E5> arbitrary5,
+    Arbitrary<E6> arbitrary6,
+    Arbitrary<E7> arbitrary7,
+    void Function(T, E1, E2, E3, E4, E5, E6, E7) action, {
+    super.dependencies,
+    super.canExecute,
+    super.precondition,
+    super.postcondition,
+    super.nextState,
+  }) : super(
+          description,
+          combine7(
+              arbitrary1,
+              arbitrary2,
+              arbitrary3,
+              arbitrary4,
+              arbitrary5,
+              arbitrary6,
+              arbitrary7,
+              (a, b, c, d, e, f, g) => (a, b, c, d, e, f, g)),
+          (s, args) => action(
+              s, args.$1, args.$2, args.$3, args.$4, args.$5, args.$6, args.$7),
+        );
+}
+
+final class Action8<T extends State, E1, E2, E3, E4, E5, E6, E7, E8>
+    extends Action<T, (E1, E2, E3, E4, E5, E6, E7, E8)> {
+  Action8(
+    String description,
+    Arbitrary<E1> arbitrary1,
+    Arbitrary<E2> arbitrary2,
+    Arbitrary<E3> arbitrary3,
+    Arbitrary<E4> arbitrary4,
+    Arbitrary<E5> arbitrary5,
+    Arbitrary<E6> arbitrary6,
+    Arbitrary<E7> arbitrary7,
+    Arbitrary<E8> arbitrary8,
+    void Function(T, E1, E2, E3, E4, E5, E6, E7, E8) action, {
+    super.dependencies,
+    super.canExecute,
+    super.precondition,
+    super.postcondition,
+    super.nextState,
+  }) : super(
+          description,
+          combine8(
+              arbitrary1,
+              arbitrary2,
+              arbitrary3,
+              arbitrary4,
+              arbitrary5,
+              arbitrary6,
+              arbitrary7,
+              arbitrary8,
+              (a, b, c, d, e, f, g, h) => (a, b, c, d, e, f, g, h)),
+          (s, args) => action(s, args.$1, args.$2, args.$3, args.$4, args.$5,
+              args.$6, args.$7, args.$8),
         );
 }
--- lib/stateful_test.dart
+++ lib/stateful_test.dart
@@ -1,5 +1,15 @@
 export 'src/state/command.dart' show Command;
-export 'src/state/command/action.dart' show Action, Action0, Action2;
+export 'src/state/command/action.dart'
+    show
+        Action,
+        Action0,
+        Action2,
+        Action3,
+        Action4,
+        Action5,
+        Action6,
+        Action7,
+        Action8;
 export 'src/state/command/finalize.dart' show Finalize;
 export 'src/state/command/initialize.dart' show Initialize;
 export 'src/state/state.dart' show Behavior, State;
