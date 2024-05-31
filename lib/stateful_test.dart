8e1d4179eeb98f88d7074c02afa43ae196bc4555
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2024-06-01 04:04:10 +0900

    塩バターパン

--- lib/stateful_test.dart
+++ lib/stateful_test.dart
@@ -10,5 +10,6 @@ export 'src/state/command/action.dart'
         Action6,
         Action7,
         Action8;
+export 'src/state/command/sequence.dart' show Sequence;
 export 'src/state/state.dart' show Behavior;
 export 'src/state/top.dart' show runBehavior;
