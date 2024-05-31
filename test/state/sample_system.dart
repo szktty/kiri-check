f2b622f23cfd17418bd6ae07a132e884db063010
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2024-05-31 16:24:57 +0900

    塩バターパン

--- test/state/sample_system.dart
+++ test/state/sample_system.dart
@@ -3,10 +3,72 @@
 // - JSONで保持
 //  - マクロを使う
 
-final class BankAccount {}
+import 'dart:convert';
+import 'sample_model.dart';
 
-final class BankAccountSystem {
-  void deposit(BankAccount account, int amount) {
+final class BankSystem {
+  final BankAccountSettings settings = BankAccountSettings();
+  final Map<int, Map<String, dynamic>> accounts = {};
+
+  void register(int id) {
+    accounts[id] = {
+      'frozen': false,
+      'balance': 0,
+      'depositPerDay': 0,
+      'withdrawPerDay': 0,
+    };
+  }
+
+  T _getProperty<T>(int id, String key) {
+    return accounts[id]![key] as T;
+  }
+
+  dynamic _setProperty(int id, String key, dynamic value) {
+    accounts[id]![key] = value;
+  }
+
+  bool frozen(int id) {
+    return _getProperty(id, 'frozen');
+  }
+
+  void nextDay() {
+    for (final account in accounts.values) {
+      account['frozen'] = false;
+      account['depositPerDay'] = 0;
+      account['withdrawPerDay'] = 0;
+    }
+  }
+
+  int getBalance(int id) {
+    return _getProperty(id, 'balance');
+  }
+
+  BankAccountResult deposit(int id, int amount) {
     // TODO
+    return BankAccountResult.success;
+  }
+
+  BankAccountResult withdraw(int id, int amount) {
+    // TODO
+
+    return BankAccountResult.success;
+  }
+
+  BankAccountResult freeze(int id) {
+    if (_getProperty(id, 'frozen')) {
+      return BankAccountResult.alreadyFrozen;
+    } else {
+      _setProperty(id, 'frozen', true);
+      return BankAccountResult.success;
+    }
+  }
+
+  BankAccountResult unfreeze(int id) {
+    if (!_getProperty(id, 'frozen')) {
+      return BankAccountResult.notFrozen;
+    } else {
+      _setProperty(id, 'frozen', false);
+      return BankAccountResult.success;
+    }
   }
 }
--- test/state/shrink_test.dart
+++ test/state/shrink_test.dart
@@ -2,25 +2,26 @@ import 'package:kiri_check/kiri_check.dart';
 import 'package:kiri_check/stateful_test.dart';
 import 'package:test/test.dart';
 
+import 'sample_behaviors.dart';
 import 'sample_model.dart';
 
 void main() {
   KiriCheck.verbosity = Verbosity.verbose;
 
   group('bank account', () {
-    /*
     property('base', () {
-      forAllStates(
+      runBehavior(
         BankAccountBehavior(),
       );
     });
-     */
 
+    /*
     property('freeze not working', () {
       runBehavior(
         BankAccountFreezeNotWorkingBehavior(),
         seed: 12345,
       );
     });
+     */
   });
 }
