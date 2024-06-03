6b0552d2c434e00735b3acdebebe56fb2ba5f518
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2024-06-03 16:58:29 +0900

    コーンスープ

---
+++ test/state/sample_test.dart
@@ -0,0 +1,16 @@
+import 'package:kiri_check/kiri_check.dart';
+import 'package:kiri_check/stateful_test.dart';
+import 'package:test/test.dart';
+
+import 'sample_behaviors.dart';
+import 'sample_model.dart';
+
+void main() {
+  KiriCheck.verbosity = Verbosity.verbose;
+
+  group('bank account', () {
+    property('base', () {
+      runBehavior(BankAccountBehavior());
+    });
+  });
+}
--- test/state/shrink_test.dart
+++ test/state/shrink_test.dart
@@ -1,30 +1,71 @@
 import 'package:kiri_check/kiri_check.dart';
 import 'package:kiri_check/stateful_test.dart';
+import 'package:test/expect.dart';
 import 'package:test/test.dart';
 
 import 'sample_behaviors.dart';
 import 'sample_model.dart';
 
+final class SubsequenceTestBehavior
+    extends Behavior<SubsequenceTestState, Null> {
+  @override
+  SubsequenceTestState createState() {
+    return SubsequenceTestState();
+  }
+
+  @override
+  Null createSystem(SubsequenceTestState state) {
+    return null;
+  }
+
+  @override
+  List<Command<SubsequenceTestState, Null>> generateCommands(
+      SubsequenceTestState state) {
+    var n = 0;
+    Command<SubsequenceTestState, Null> increment([int value = 1]) {
+      n++;
+      return Action0('increment $value ($n)', (s, system) {
+        s.increment(value);
+      }, postcondition: (s, system) => s.value < 100);
+    }
+
+    return [
+      Sequence('sequence', [
+        increment(),
+        increment(),
+        increment(),
+        increment(),
+        increment(),
+        increment(),
+        increment(),
+        increment(40),
+        increment(),
+        increment(40),
+        increment(),
+        increment(40),
+      ]),
+    ];
+  }
+}
+
+final class SubsequenceTestState {
+  int value = 0;
+
+  void increment(int value) {
+    this.value += value;
+  }
+}
+
 void main() {
   KiriCheck.verbosity = Verbosity.verbose;
 
-  group('bank account', () {
-    property('base', () {
-      runBehavior(
-        BankAccountBehavior(),
-        onFalsify: (example) {
-          print('onFalsify example: ${example}');
-        },
-      );
-    });
-
-    /*
-    property('freeze not working', () {
-      runBehavior(
-        BankAccountFreezeNotWorkingBehavior(),
-        seed: 12345,
-      );
-    });
-     */
+  property('subsequence', () {
+    runBehavior(
+      SubsequenceTestBehavior(),
+      onFalsify: (example) {
+        expect(example.steps.length, lessThanOrEqualTo(5));
+      },
+      ignoreFalsify: true,
+    );
   });
 }
