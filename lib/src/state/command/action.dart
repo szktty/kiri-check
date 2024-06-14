d84ec7b45d3e93b740f26ce5bc05513742e6f83a
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2024-06-14 16:00:52 +0900

    だんご

--- lib/src/state/command/action.dart
+++ lib/src/state/command/action.dart
@@ -114,12 +114,316 @@ final class Action2<State, System, T1, T2, R>
     Arbitrary<T2> arbitrary2, {
     required void Function(State, T1, T2) nextState,
     required R Function(System, T1, T2) run,
-    super.precondition,
-    super.postcondition,
+    bool Function(State, T1, T2)? precondition,
+    bool Function(State, T1, T2, R)? postcondition,
   }) : super(
           description,
           combine2(arbitrary1, arbitrary2, (a, b) => (a, b)),
           nextState: (s, args) => nextState(s, args.$1, args.$2),
           run: (sys, args) => run(sys, args.$1, args.$2),
+          precondition: (s, args) =>
+              precondition?.call(s, args.$1, args.$2) ?? true,
+          postcondition: (s, args, r) =>
+              postcondition?.call(s, args.$1, args.$2, r) ?? true,
+        );
+}
+
+/// A command [Action] with 3 arbitraries.
+final class Action3<State, System, T1, T2, T3, R>
+    extends Action<State, System, (T1, T2, T3), R> {
+  /// Creates a new action command with 3 arbitraries.
+  ///
+  /// Parameters:
+  /// - `description`: The description of the action.
+  /// - `arbitrary1`: The arbitrary used to generate the first value.
+  /// - `arbitrary2`: The arbitrary used to generate the second value.
+  /// - `arbitrary3`: The arbitrary used to generate the third value.
+  /// - `run`: A function to perform the action.
+  /// - `precondition`: A function to test the precondition of the action.
+  /// - `postcondition`: A function to test the postcondition of the action.
+  Action3(
+    String description,
+    Arbitrary<T1> arbitrary1,
+    Arbitrary<T2> arbitrary2,
+    Arbitrary<T3> arbitrary3, {
+    required void Function(State, T1, T2, T3) nextState,
+    required R Function(System, T1, T2, T3) run,
+    bool Function(State, T1, T2, T3)? precondition,
+    bool Function(State, T1, T2, T3, R)? postcondition,
+  }) : super(
+          description,
+          combine3(arbitrary1, arbitrary2, arbitrary3, (a, b, c) => (a, b, c)),
+          nextState: (s, args) => nextState(s, args.$1, args.$2, args.$3),
+          run: (sys, args) => run(sys, args.$1, args.$2, args.$3),
+          precondition: (s, args) =>
+              precondition?.call(s, args.$1, args.$2, args.$3) ?? true,
+          postcondition: (s, args, r) =>
+              postcondition?.call(s, args.$1, args.$2, args.$3, r) ?? true,
+        );
+}
+
+/// A command [Action] with 4 arbitraries.
+final class Action4<State, System, T1, T2, T3, T4, R>
+    extends Action<State, System, (T1, T2, T3, T4), R> {
+  /// Creates a new action command with 4 arbitraries.
+  ///
+  /// Parameters:
+  /// - `description`: The description of the action.
+  /// - `arbitrary1`: The arbitrary used to generate the first value.
+  /// - `arbitrary2`: The arbitrary used to generate the second value.
+  /// - `arbitrary3`: The arbitrary used to generate the third value.
+  /// - `arbitrary4`: The arbitrary used to generate the fourth value.
+  /// - `run`: A function to perform the action.
+  /// - `precondition`: A function to test the precondition of the action.
+  /// - `postcondition`: A function to test the postcondition of the action.
+  Action4(
+    String description,
+    Arbitrary<T1> arbitrary1,
+    Arbitrary<T2> arbitrary2,
+    Arbitrary<T3> arbitrary3,
+    Arbitrary<T4> arbitrary4, {
+    required void Function(State, T1, T2, T3, T4) nextState,
+    required R Function(System, T1, T2, T3, T4) run,
+    bool Function(State, T1, T2, T3, T4)? precondition,
+    bool Function(State, T1, T2, T3, T4, R)? postcondition,
+  }) : super(
+          description,
+          combine4(
+            arbitrary1,
+            arbitrary2,
+            arbitrary3,
+            arbitrary4,
+            (a, b, c, d) => (a, b, c, d),
+          ),
+          nextState: (s, args) =>
+              nextState(s, args.$1, args.$2, args.$3, args.$4),
+          run: (sys, args) => run(sys, args.$1, args.$2, args.$3, args.$4),
+          precondition: (s, args) =>
+              precondition?.call(s, args.$1, args.$2, args.$3, args.$4) ?? true,
+          postcondition: (s, args, r) =>
+              postcondition?.call(s, args.$1, args.$2, args.$3, args.$4, r) ??
+              true,
+        );
+}
+
+/// A command [Action] with 5 arbitraries.
+final class Action5<State, System, T1, T2, T3, T4, T5, R>
+    extends Action<State, System, (T1, T2, T3, T4, T5), R> {
+  /// Creates a new action command with 5 arbitraries.
+  ///
+  /// Parameters:
+  /// - `description`: The description of the action.
+  /// - `arbitrary1`: The arbitrary used to generate the first value.
+  /// - `arbitrary2`: The arbitrary used to generate the second value.
+  /// - `arbitrary3`: The arbitrary used to generate the third value.
+  /// - `arbitrary4`: The arbitrary used to generate the fourth value.
+  /// - `arbitrary5`: The arbitrary used to generate the fifth value.
+  /// - `run`: A function to perform the action.
+  /// - `precondition`: A function to test the precondition of the action.
+  /// - `postcondition`: A function to test the postcondition of the action.
+  Action5(
+    String description,
+    Arbitrary<T1> arbitrary1,
+    Arbitrary<T2> arbitrary2,
+    Arbitrary<T3> arbitrary3,
+    Arbitrary<T4> arbitrary4,
+    Arbitrary<T5> arbitrary5, {
+    required void Function(State, T1, T2, T3, T4, T5) nextState,
+    required R Function(System, T1, T2, T3, T4, T5) run,
+    bool Function(State, T1, T2, T3, T4, T5)? precondition,
+    bool Function(State, T1, T2, T3, T4, T5, R)? postcondition,
+  }) : super(
+          description,
+          combine5(
+            arbitrary1,
+            arbitrary2,
+            arbitrary3,
+            arbitrary4,
+            arbitrary5,
+            (a, b, c, d, e) => (a, b, c, d, e),
+          ),
+          nextState: (s, args) =>
+              nextState(s, args.$1, args.$2, args.$3, args.$4, args.$5),
+          run: (sys, args) =>
+              run(sys, args.$1, args.$2, args.$3, args.$4, args.$5),
+          precondition: (s, args) =>
+              precondition?.call(
+                  s, args.$1, args.$2, args.$3, args.$4, args.$5) ??
+              true,
+          postcondition: (s, args, r) =>
+              postcondition?.call(
+                  s, args.$1, args.$2, args.$3, args.$4, args.$5, r) ??
+              true,
+        );
+}
+
+/// A command [Action] with 6 arbitraries.
+final class Action6<State, System, T1, T2, T3, T4, T5, T6, R>
+    extends Action<State, System, (T1, T2, T3, T4, T5, T6), R> {
+  /// Creates a new action command with 6 arbitraries.
+  ///
+  /// Parameters:
+  /// - `description`: The description of the action.
+  /// - `arbitrary1`: The arbitrary used to generate the first value.
+  /// - `arbitrary2`: The arbitrary used to generate the second value.
+  /// - `arbitrary3`: The arbitrary used to generate the third value.
+  /// - `arbitrary4`: The arbitrary used to generate the fourth value.
+  /// - `arbitrary5`: The arbitrary used to generate the fifth value.
+  /// - `arbitrary6`: The arbitrary used to generate the sixth value.
+  /// - `run`: A function to perform the action.
+  /// - `precondition`: A function to test the precondition of the action.
+  /// - `postcondition`: A function to test the postcondition of the action.
+  Action6(
+    String description,
+    Arbitrary<T1> arbitrary1,
+    Arbitrary<T2> arbitrary2,
+    Arbitrary<T3> arbitrary3,
+    Arbitrary<T4> arbitrary4,
+    Arbitrary<T5> arbitrary5,
+    Arbitrary<T6> arbitrary6, {
+    required void Function(State, T1, T2, T3, T4, T5, T6) nextState,
+    required R Function(System, T1, T2, T3, T4, T5, T6) run,
+    bool Function(State, T1, T2, T3, T4, T5, T6)? precondition,
+    bool Function(State, T1, T2, T3, T4, T5, T6, R)? postcondition,
+  }) : super(
+          description,
+          combine6(
+            arbitrary1,
+            arbitrary2,
+            arbitrary3,
+            arbitrary4,
+            arbitrary5,
+            arbitrary6,
+            (a, b, c, d, e, f) => (a, b, c, d, e, f),
+          ),
+          nextState: (s, args) => nextState(
+              s, args.$1, args.$2, args.$3, args.$4, args.$5, args.$6),
+          run: (sys, args) =>
+              run(sys, args.$1, args.$2, args.$3, args.$4, args.$5, args.$6),
+          precondition: (s, args) =>
+              precondition?.call(
+                  s, args.$1, args.$2, args.$3, args.$4, args.$5, args.$6) ??
+              true,
+          postcondition: (s, args, r) =>
+              postcondition?.call(
+                  s, args.$1, args.$2, args.$3, args.$4, args.$5, args.$6, r) ??
+              true,
+        );
+}
+
+/// A command [Action] with 7 arbitraries.
+final class Action7<State, System, T1, T2, T3, T4, T5, T6, T7, R>
+    extends Action<State, System, (T1, T2, T3, T4, T5, T6, T7), R> {
+  /// Creates a new action command with 7 arbitraries.
+  ///
+  /// Parameters:
+  /// - `description`: The description of the action.
+  /// - `arbitrary1`: The arbitrary used to generate the first value.
+  /// - `arbitrary2`: The arbitrary used to generate the second value.
+  /// - `arbitrary3`: The arbitrary used to generate the third value.
+  /// - `arbitrary4`: The arbitrary used to generate the fourth value.
+  /// - `arbitrary5`: The arbitrary used to generate the fifth value.
+  /// - `arbitrary6`: The arbitrary used to generate the sixth value.
+  /// - `arbitrary7`: The arbitrary used to generate the seventh value.
+  /// - `run`: A function to perform the action.
+  /// - `precondition`: A function to test the precondition of the action.
+  /// - `postcondition`: A function to test the postcondition of the action.
+  Action7(
+    String description,
+    Arbitrary<T1> arbitrary1,
+    Arbitrary<T2> arbitrary2,
+    Arbitrary<T3> arbitrary3,
+    Arbitrary<T4> arbitrary4,
+    Arbitrary<T5> arbitrary5,
+    Arbitrary<T6> arbitrary6,
+    Arbitrary<T7> arbitrary7, {
+    required void Function(State, T1, T2, T3, T4, T5, T6, T7) nextState,
+    required R Function(System, T1, T2, T3, T4, T5, T6, T7) run,
+    bool Function(State, T1, T2, T3, T4, T5, T6, T7)? precondition,
+    bool Function(State, T1, T2, T3, T4, T5, T6, T7, R)? postcondition,
+  }) : super(
+          description,
+          combine7(
+            arbitrary1,
+            arbitrary2,
+            arbitrary3,
+            arbitrary4,
+            arbitrary5,
+            arbitrary6,
+            arbitrary7,
+            (a, b, c, d, e, f, g) => (a, b, c, d, e, f, g),
+          ),
+          nextState: (s, args) => nextState(
+              s, args.$1, args.$2, args.$3, args.$4, args.$5, args.$6, args.$7),
+          run: (sys, args) => run(sys, args.$1, args.$2, args.$3, args.$4,
+              args.$5, args.$6, args.$7),
+          precondition: (s, args) =>
+              precondition?.call(s, args.$1, args.$2, args.$3, args.$4, args.$5,
+                  args.$6, args.$7) ??
+              true,
+          postcondition: (s, args, r) =>
+              postcondition?.call(s, args.$1, args.$2, args.$3, args.$4,
+                  args.$5, args.$6, args.$7, r) ??
+              true,
+        );
+}
+
+/// A command [Action] with 8 arbitraries.
+final class Action8<State, System, T1, T2, T3, T4, T5, T6, T7, T8, R>
+    extends Action<State, System, (T1, T2, T3, T4, T5, T6, T7, T8), R> {
+  /// Creates a new action command with 8 arbitraries.
+  ///
+  /// Parameters:
+  /// - `description`: The description of the action.
+  /// - `arbitrary1`: The arbitrary used to generate the first value.
+  /// - `arbitrary2`: The arbitrary used to generate the second value.
+  /// - `arbitrary3`: The arbitrary used to generate the third value.
+  /// - `arbitrary4`: The arbitrary used to generate the fourth value.
+  /// - `arbitrary5`: The arbitrary used to generate the fifth value.
+  /// - `arbitrary6`: The arbitrary used to generate the sixth value.
+  /// - `arbitrary7`: The arbitrary used to generate the seventh value.
+  /// - `arbitrary8`: The arbitrary used to generate the eighth value.
+  /// - `run`: A function to perform the action.
+  /// - `precondition`: A function to test the precondition of the action.
+  /// - `postcondition`: A function to test the postcondition of the action.
+  Action8(
+    String description,
+    Arbitrary<T1> arbitrary1,
+    Arbitrary<T2> arbitrary2,
+    Arbitrary<T3> arbitrary3,
+    Arbitrary<T4> arbitrary4,
+    Arbitrary<T5> arbitrary5,
+    Arbitrary<T6> arbitrary6,
+    Arbitrary<T7> arbitrary7,
+    Arbitrary<T8> arbitrary8, {
+    required void Function(State, T1, T2, T3, T4, T5, T6, T7, T8) nextState,
+    required R Function(System, T1, T2, T3, T4, T5, T6, T7, T8) run,
+    bool Function(State, T1, T2, T3, T4, T5, T6, T7, T8)? precondition,
+    bool Function(State, T1, T2, T3, T4, T5, T6, T7, T8, R)? postcondition,
+  }) : super(
+          description,
+          combine8(
+            arbitrary1,
+            arbitrary2,
+            arbitrary3,
+            arbitrary4,
+            arbitrary5,
+            arbitrary6,
+            arbitrary7,
+            arbitrary8,
+            (a, b, c, d, e, f, g, h) => (a, b, c, d, e, f, g, h),
+          ),
+          nextState: (s, args) => nextState(s, args.$1, args.$2, args.$3,
+              args.$4, args.$5, args.$6, args.$7, args.$8),
+          run: (sys, args) => run(sys, args.$1, args.$2, args.$3, args.$4,
+              args.$5, args.$6, args.$7, args.$8),
+          precondition: (s, args) =>
+              precondition?.call(s, args.$1, args.$2, args.$3, args.$4, args.$5,
+                  args.$6, args.$7, args.$8) ??
+              true,
+          postcondition: (s, args, r) =>
+              postcondition?.call(s, args.$1, args.$2, args.$3, args.$4,
+                  args.$5, args.$6, args.$7, args.$8, r) ??
+              true,
         );
 }
--- test/state/command/action_test.dart
+++ test/state/command/action_test.dart
@@ -98,6 +98,247 @@ final class Action1TestBehavior extends ActionTestBehaviorBase {
   }
 }
 
+final class Action2TestBehavior extends ActionTestBehaviorBase {
+  @override
+  List<Command<ConditionalTestState, ConditionalTestSystem>> generateCommands(
+    ConditionalTestState s,
+  ) {
+    return [
+      Action2(
+        'Action',
+        integer(),
+        integer(),
+        nextState: (s, v1, v2) {
+          expect(s.precondition, isTrue);
+          s.nextState = true;
+        },
+        run: (system, v1, v2) {
+          system.run = true;
+          return (v1, v2);
+        },
+        precondition: (s, v1, v2) {
+          s.result = (v1, v2);
+          return s.precondition = true;
+        },
+        postcondition: (s, v1, v2, result) {
+          return s.precondition && s.result == result && result == (v1, v2);
+        },
+      ),
+    ];
+  }
+}
+
+final class Action3TestBehavior extends ActionTestBehaviorBase {
+  @override
+  List<Command<ConditionalTestState, ConditionalTestSystem>> generateCommands(
+    ConditionalTestState s,
+  ) {
+    return [
+      Action3(
+        'Action',
+        integer(),
+        integer(),
+        integer(),
+        nextState: (s, v1, v2, v3) {
+          expect(s.precondition, isTrue);
+          s.nextState = true;
+        },
+        run: (system, v1, v2, v3) {
+          system.run = true;
+          return (v1, v2, v3);
+        },
+        precondition: (s, v1, v2, v3) {
+          s.result = (v1, v2, v3);
+          return s.precondition = true;
+        },
+        postcondition: (s, v1, v2, v3, result) {
+          return s.precondition && s.result == result && result == (v1, v2, v3);
+        },
+      ),
+    ];
+  }
+}
+
+final class Action4TestBehavior extends ActionTestBehaviorBase {
+  @override
+  List<Command<ConditionalTestState, ConditionalTestSystem>> generateCommands(
+    ConditionalTestState s,
+  ) {
+    return [
+      Action4(
+        'Action',
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        nextState: (s, v1, v2, v3, v4) {
+          expect(s.precondition, isTrue);
+          s.nextState = true;
+        },
+        run: (system, v1, v2, v3, v4) {
+          system.run = true;
+          return (v1, v2, v3, v4);
+        },
+        precondition: (s, v1, v2, v3, v4) {
+          s.result = (v1, v2, v3, v4);
+          return s.precondition = true;
+        },
+        postcondition: (s, v1, v2, v3, v4, result) {
+          return s.precondition &&
+              s.result == result &&
+              result == (v1, v2, v3, v4);
+        },
+      ),
+    ];
+  }
+}
+
+final class Action5TestBehavior extends ActionTestBehaviorBase {
+  @override
+  List<Command<ConditionalTestState, ConditionalTestSystem>> generateCommands(
+    ConditionalTestState s,
+  ) {
+    return [
+      Action5(
+        'Action',
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        nextState: (s, v1, v2, v3, v4, v5) {
+          expect(s.precondition, isTrue);
+          s.nextState = true;
+        },
+        run: (system, v1, v2, v3, v4, v5) {
+          system.run = true;
+          return (v1, v2, v3, v4, v5);
+        },
+        precondition: (s, v1, v2, v3, v4, v5) {
+          s.result = (v1, v2, v3, v4, v5);
+          return s.precondition = true;
+        },
+        postcondition: (s, v1, v2, v3, v4, v5, result) {
+          return s.precondition &&
+              s.result == result &&
+              result == (v1, v2, v3, v4, v5);
+        },
+      ),
+    ];
+  }
+}
+
+final class Action6TestBehavior extends ActionTestBehaviorBase {
+  @override
+  List<Command<ConditionalTestState, ConditionalTestSystem>> generateCommands(
+    ConditionalTestState s,
+  ) {
+    return [
+      Action6(
+        'Action',
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        nextState: (s, v1, v2, v3, v4, v5, v6) {
+          expect(s.precondition, isTrue);
+          s.nextState = true;
+        },
+        run: (system, v1, v2, v3, v4, v5, v6) {
+          system.run = true;
+          return (v1, v2, v3, v4, v5, v6);
+        },
+        precondition: (s, v1, v2, v3, v4, v5, v6) {
+          s.result = (v1, v2, v3, v4, v5, v6);
+          return s.precondition = true;
+        },
+        postcondition: (s, v1, v2, v3, v4, v5, v6, result) {
+          return s.precondition &&
+              s.result == result &&
+              result == (v1, v2, v3, v4, v5, v6);
+        },
+      ),
+    ];
+  }
+}
+
+final class Action7TestBehavior extends ActionTestBehaviorBase {
+  @override
+  List<Command<ConditionalTestState, ConditionalTestSystem>> generateCommands(
+    ConditionalTestState s,
+  ) {
+    return [
+      Action7(
+        'Action',
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        nextState: (s, v1, v2, v3, v4, v5, v6, v7) {
+          expect(s.precondition, isTrue);
+          s.nextState = true;
+        },
+        run: (system, v1, v2, v3, v4, v5, v6, v7) {
+          system.run = true;
+          return (v1, v2, v3, v4, v5, v6, v7);
+        },
+        precondition: (s, v1, v2, v3, v4, v5, v6, v7) {
+          s.result = (v1, v2, v3, v4, v5, v6, v7);
+          return s.precondition = true;
+        },
+        postcondition: (s, v1, v2, v3, v4, v5, v6, v7, result) {
+          return s.precondition &&
+              s.result == result &&
+              result == (v1, v2, v3, v4, v5, v6, v7);
+        },
+      ),
+    ];
+  }
+}
+
+final class Action8TestBehavior extends ActionTestBehaviorBase {
+  @override
+  List<Command<ConditionalTestState, ConditionalTestSystem>> generateCommands(
+    ConditionalTestState s,
+  ) {
+    return [
+      Action8(
+        'Action',
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        integer(),
+        nextState: (s, v1, v2, v3, v4, v5, v6, v7, v8) {
+          expect(s.precondition, isTrue);
+          s.nextState = true;
+        },
+        run: (system, v1, v2, v3, v4, v5, v6, v7, v8) {
+          system.run = true;
+          return (v1, v2, v3, v4, v5, v6, v7, v8);
+        },
+        precondition: (s, v1, v2, v3, v4, v5, v6, v7, v8) {
+          s.result = (v1, v2, v3, v4, v5, v6, v7, v8);
+          return s.precondition = true;
+        },
+        postcondition: (s, v1, v2, v3, v4, v5, v6, v7, v8, result) {
+          return s.precondition &&
+              s.result == result &&
+              result == (v1, v2, v3, v4, v5, v6, v7, v8);
+        },
+      ),
+    ];
+  }
+}
+
 void main() {
   property('Action0', () {
     runBehavior(
@@ -110,6 +351,62 @@ void main() {
   property('Action1', () {
     runBehavior(
       Action1TestBehavior(),
+      maxCycles: 10,
+      maxSteps: 10,
+    );
+  });
+
+  property('Action2', () {
+    runBehavior(
+      Action2TestBehavior(),
+      maxCycles: 10,
+      maxSteps: 10,
+    );
+  });
+
+  property('Action3', () {
+    runBehavior(
+      Action3TestBehavior(),
+      maxCycles: 10,
+      maxSteps: 10,
+    );
+  });
+
+  property('Action4', () {
+    runBehavior(
+      Action4TestBehavior(),
+      maxCycles: 10,
+      maxSteps: 10,
+    );
+  });
+
+  property('Action5', () {
+    runBehavior(
+      Action5TestBehavior(),
+      maxCycles: 10,
+      maxSteps: 10,
+    );
+  });
+
+  property('Action6', () {
+    runBehavior(
+      Action6TestBehavior(),
+      maxCycles: 10,
+      maxSteps: 10,
+    );
+  });
+
+  property('Action7', () {
+    runBehavior(
+      Action7TestBehavior(),
+      maxCycles: 10,
+      maxSteps: 10,
+    );
+  });
+
+  property('Action8', () {
+    runBehavior(
+      Action8TestBehavior(),
       maxCycles: 10,
       maxSteps: 10,
     );
